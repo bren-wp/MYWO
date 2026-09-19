84dc39b57b4ea371e983fdc568b2079ea56fbfa6
Brendigo <info@brendigo.com>
2026-09-20 00:25:34 +0200

    Align updater fallback version with 0.9.3

--- src/MYWO.Updater/Program.cs
+++ src/MYWO.Updater/Program.cs
@@ -186,7 +186,7 @@ internal static class Program
     private static HttpClient CreateHttpClient()
     {
         var http = new HttpClient { Timeout = TimeSpan.FromSeconds(45) };
-        var updaterVersion = typeof(Program).Assembly.GetName().Version?.ToString(3) ?? "0.9.0";
+        var updaterVersion = typeof(Program).Assembly.GetName().Version?.ToString(3) ?? "0.9.3";
         http.DefaultRequestHeaders.UserAgent.ParseAdd($"MYWO-Updater/{updaterVersion}");
         return http;
     }
